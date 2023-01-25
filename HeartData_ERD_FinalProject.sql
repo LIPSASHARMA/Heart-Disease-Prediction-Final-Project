0ce6424e2abcdd14bb6903f808fc2b3f15ef7da2
lipsasharma <79360743+LIPSASHARMA@users.noreply.github.com>
2023-01-25 17:22:16 -0500

    Updated ERD diagram and snapshot

--- HeartData_ERD_FinalProject.sql
+++ HeartData_ERD_FinalProject.sql
@@ -1,6 +1,8 @@
 ﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
+-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
 CREATE TABLE "PatientPersonalDetails" (
     "PatientID" int   NOT NULL,
@@ -22,28 +24,43 @@ CREATE TABLE "PatientHeartInfo" (
     "ExerciseAngina" char   NOT NULL,
     "Oldpeak" float   NOT NULL,
     "ST_Slope" varchar(20)   NOT NULL,
+    CONSTRAINT "pk_PatientHeartInfo" PRIMARY KEY (
+        "PatientID"
+     )
+);
+
+CREATE TABLE "PatientDetails" (
+    "PatientID" integer   NOT NULL,
+    "Age" integer   NOT NULL,
+    "Sex" char   NOT NULL,
+    "ChestPainType" varchar(20)   NOT NULL,
+    "RestingBP" integer   NOT NULL,
+    "Cholesterol" integer   NOT NULL,
+    "FastingBS" boolean   NOT NULL,
+    "RestingECG" varchar(20)   NOT NULL,
+    "MaxHR" integer   NOT NULL,
+    "ExerciseAngina" char   NOT NULL,
+    "Oldpeak" float   NOT NULL,
+    "ST_Slope" varchar(20)   NOT NULL,
     "HeartDisease" boolean   NOT NULL
+);
+
+CREATE TABLE "PatientHeartDiseaseOutput" (
+    "PatientID" int   NOT NULL,
+    "HeartDisease" boolean   NOT NULL,
+    CONSTRAINT "pk_PatientHeartDiseaseOutput" PRIMARY KEY (
+        "PatientID"
+     )
 );
 
 ALTER TABLE "PatientHeartInfo" ADD CONSTRAINT "fk_PatientHeartInfo_PatientID" FOREIGN KEY("PatientID")
 REFERENCES "PatientPersonalDetails" ("PatientID");
 
+ALTER TABLE "PatientHeartDiseaseOutput" ADD CONSTRAINT "fk_PatientHeartDiseaseOutput_PatientID" FOREIGN KEY("PatientID")
+REFERENCES "PatientPersonalDetails" ("PatientID");
 
 
-CREATE TABLE PatientDetails (
-	PatientID integer, 
-	Age integer, 
-	Sex char, 
-	ChestPainType varchar(20), 
-	RestingBP integer, 
-	Cholesterol integer, 
-	FastingBS boolean, 
-	RestingECG varchar(20), 
-	MaxHR integer, 
-	ExerciseAngina char, 
-	Oldpeak float, 
-	ST_Slope varchar(20), 
-	HeartDisease boolean);
+
 
 COPY patientdetails(PatientID, Age, Sex, ChestPainType, RestingBP, Cholesterol, FastingBS, RestingECG, MaxHR, ExerciseAngina, Oldpeak, ST_Slope, HeartDisease)
 FROM 'C:\\Users\\lipsa\\UoT-Data-Analytics\\final-project-\\archive\\heart.csv'
@@ -57,3 +74,19 @@ INSERT INTO public."PatientHeartInfo"("PatientID","ChestPainType","RestingBP","C
 	
 INSERT INTO public."PatientPersonalDetails"("PatientID","Age","Sex") 
 	select PatientID, Age, Sex from patientdetails;
+
+
+CREATE TABLE "PatientHeartDiseaseOutput" (
+    "PatientID" int   NOT NULL,
+    "HeartDisease" boolean   NOT NULL,
+    CONSTRAINT "pk_PatientHeartDiseaseOutput" PRIMARY KEY (
+        "PatientID"
+     )
+);
+
+INSERT INTO public."PatientHeartDiseaseOutput"("PatientID","HeartDisease") 
+	select PatientID, HeartDisease from patientdetails;
+
+
+
+
